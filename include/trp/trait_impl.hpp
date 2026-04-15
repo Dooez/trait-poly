07faabfb0ca08faa222d2029d4907a3675814402
Timofey Dovgal <dooezzz@gmail.com>
2026-04-15 12:41:42 +0300

    Clean

--- include/trp/trait_impl.hpp
+++ include/trp/trait_impl.hpp
@@ -125,9 +125,6 @@ struct method_invoker {
     auto operator()(Args&&... args) const
         volatile noexcept(noexcept_cvm_invoker<CVMInvoker, typename TRef::vtable_t, Args...>)
             -> decltype(auto) {
-        // CVMInvoker invoker{};
-        // auto*      vtptr   = get_trait_ref().vtable_ptr_;
-        // void*      obj_ptr = get_trait_ref().obj_ptr_;
         return CVMInvoker{}(
             get_trait_ref().vtable_ptr_, get_trait_ref().obj_ptr_, std::forward<Args>(args)...);
     }
