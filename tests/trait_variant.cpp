f3fe68b9c41c649e4aef5e67de4ed1e951a9d349
Timofey Dovgal <dooezzz@gmail.com>
2026-06-15 13:35:25 +0300

    fix accidential slip

--- tests/trait_variant.cpp
+++ tests/trait_variant.cpp
@@ -63,7 +63,6 @@ static_assert(std::same_as<decltype(emplace<second_impl>(std::declval<variant_t&
 
 int main() {
     auto direct = variant_t(first_impl{5});
-extract_vtable_ptr(direct);
     test::expect_eq("trait_variant", "direct value ctor index", static_cast<int>(index(direct)), 0);
     test::expect_eq("trait_variant", "direct value ctor", direct.value(), 5);
 
